f96af73183f836db85e5a96f53b5c9c06b02de79
samillee <samillee@allif.co.kr>
2016-09-23 13:20:24 +0900

    task 소스 추가 및 문서 정리

--- hello/hello.go
+++ hello/hello.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/samillee/go_study/stringutil"
 )
 
 func main() {
 	fmt.Printf("hello, world\n")
-	fmt.Printf(stringutil.Reverse("hello, world\n"))
+	a := "이삼일"
+	fmt.Printf(stringutil.Reverse(a))
 }
